9b0d83fdfed725a6ed70033fc07cee8c6dfda9c3
Shlomi Nissan <admin@betamark.com>
2024-07-01 13:30:22 -0700

    Add WebGL context

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { WebGLApplication, WebGLContext } from './webgl/webgl_application'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const app = new WebGLApplication("#window");
+
+app.initialize((_: WebGLContext) => {
+  // TODO: initalize webgl application  
+});
+
+app.tick((gl: WebGLContext, _: number) => {
+  gl.clearColor(0.0, 0.0, 0.5, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // TODO: update/draw
+});
---
+++ src/webgl/webgl_application.ts
@@ -0,0 +1,58 @@
+export type WebGLContext = WebGLRenderingContext | WebGL2RenderingContext;
+
+export class WebGLApplication {
+    private gl: WebGLContext | null = null;
+    private lastTimestamp = 0;
+
+    private checkError() {
+        const error = this.context().getError();
+        if (error !== this.context().NO_ERROR) {
+            console.error('WebGL error:', error);
+        }
+    }
+
+    private context() {
+        if (this.gl == null) {
+            throw new Error(`Failed to initialize WebGL application`);
+        }
+        return this.gl;
+    }
+
+    constructor(selector: string) {
+        const container = document.querySelector<HTMLElement>(selector);
+        if (!container) {
+            throw new Error(`Selector '${selector}' is not a valid HTML element`)
+        }
+        container.innerHTML = `<canvas id="webgl-ca011f2b319"></canvas>`;
+
+        const canvas = document.querySelector<HTMLCanvasElement>(`canvas`);
+        if (!canvas) {
+            throw new Error(`Failed to inject a Canvas element into '${selector}'`);
+        }
+
+        this.gl = canvas.getContext(`webgl2`) || canvas.getContext(`webgl`);
+        if (this.gl) {
+            console.log(`WebGL version ${this.gl.getParameter(this.gl.VERSION)}`)
+        } else {
+            throw new Error(`Failed to initialize WebGL`);
+        }
+
+        this.gl.viewport(0, 0, canvas.clientWidth, canvas.clientHeight);
+        this.checkError();
+    }
+
+    initialize(callback: (gl: WebGLContext) => void) {
+        callback(this.context());
+    }
+
+    tick(callback: (gl: WebGLContext, dt: number) => void) {
+        const tick = (timestamp: number) => {
+            const dt = timestamp - this.lastTimestamp;
+            this.lastTimestamp = timestamp;
+            callback(this.context(), dt);
+            requestAnimationFrame(tick);
+            this.checkError();
+        };
+        requestAnimationFrame(tick); 
+    }
+};
